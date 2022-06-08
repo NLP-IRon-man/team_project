056a07549e189ffe39750a617f64a2ada38072c3
Seongchan Joe <wordnumperiod@gmail.com>
2022-06-09 03:59:32 +0900

    Refactoring the code to remove stop words.

--- code/remove_stop_words.py
+++ code/remove_stop_words.py
@@ -9,16 +9,16 @@ import pandas as pd
 import os
 import copy
 
-scripts_path = "./scripts"
-scripts_name = os.listdir(scripts_path)
+csv_dir_path = "./code/csv"
+csv_file_name_list = os.listdir(csv_dir_path)
 movie_data_dict = {}
-for script_name in scripts_name:
-    csv_name = Path(script_name).with_suffix(".csv").name
-    csv_path = './csv/' + csv_name
-    movie_data = pd.read_csv(csv_path).to_dict()
-    movie_data_dict[Path(script_name).with_suffix("")] = movie_data
+for csv_file_name in csv_file_name_list:
+    csv_file_path = f"{csv_dir_path}/{csv_file_name}"
+    movie_data = pd.read_csv(csv_file_path).to_dict()
+    movie_data_dict[Path(csv_file_name).with_suffix("").name] = movie_data
 
-movie_data_dict = {k: {k1: v1[0] for k1, v1 in v.items()} for k, v in movie_data_dict.items()}
+movie_data_dict = {k: {k1: v1[0] for k1, v1 in v.items()}
+                   for k, v in movie_data_dict.items()}
 for _, v in movie_data_dict.items():
     del (v["Unnamed: 0"])
 stop_words = set(stopwords.words("english"))
@@ -53,29 +53,26 @@ def process_data(movie_data_dict: Dict, fn):
 
 
 def save_data(movie_data_dict: Dict):
-    copied_data_dict = copy.deepcopy(movie_data_dict)
-    for name, data in copied_data_dict.items():
-        for character, token_list in data.items():
-            data[character] = " ".join(token_list)
-    for name, data in copied_data_dict.items():
-        csv_path = f"./csv/{name}_preprocess.csv"
-        DataFrame.from_dict([data]).to_csv(csv_path)
+    for name, _ in movie_data_dict.items():
+        csv_path = f"./code/preprocess/{name}_preprocess.csv"
+        DataFrame.from_dict(movie_data_dict, orient="index").to_csv(csv_path)
 
 
-def read_data(csv_path:str)->Dict:
-    return pd.read_csv(csv_path, index_col = 0).to_dict()
+def read_data(csv_path: str) -> Dict:
+    return pd.read_csv(csv_path).to_dict()
+
 
 process_data(movie_data_dict, tokenize)
 print("\n**After tokenizing")
-print_movie_data(movie_data_dict)
+# print_movie_data(movie_data_dict)
 
 process_data(movie_data_dict, lemmatize)
 print("\n**After lemmatizing")
-print_movie_data(movie_data_dict)
+# print_movie_data(movie_data_dict)
 
 process_data(movie_data_dict, remove_stop_words)
 print("\n**After removing stop words")
-print_movie_data(movie_data_dict)
+# print_movie_data(movie_data_dict)
 save_data(movie_data_dict)
 
-dict = read_data("./csv/Up_preprocess.csv")
+dict = read_data("./code/preprocess/Up_preprocess.csv")
